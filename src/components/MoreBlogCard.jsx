1a1c4e051739a6e88a83f9998d3551a68b0c384b
Mohammed Ameer <mohdameer788@gmail.com>
2023-12-06 22:39:38 +0530

    blog page updated

--- src/components/MoreBlogCard.jsx
+++ src/components/MoreBlogCard.jsx
@@ -5,7 +5,7 @@ export default function MoreBlogCard({title,category,color,image,id}){
         <div className="flex items-center justify-center  flex-col -space-y-3 col-span-12 sm:col-span-6 xl:col-span-3">
                 <img src={image} alt="" />
                 <div className=" bg-white w-[62%] sm:w-[81%] lg:w-[68%] xl:w-auto capitalize px-4  py-[6%] flex flex-col gap-2 rounded-xl">
-                <div className={`${color?`bg-[${color}]`:''} py-1 px-4 font-semibold text-xs rounded-full w-fit text-white uppercase`}>
+                <div style={{ backgroundColor: color }} className={` py-1 px-4 font-semibold text-xs rounded-full w-fit text-white uppercase`}>
                 {category}
             </div>
                    <div className=" text-xl font-h2-tag-heading   lg:w-[70%]">
